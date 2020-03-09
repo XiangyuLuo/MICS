dff86da8df919bd36e8b98f830cb543be4f9feb3
Xiangyu Luo <xyluo1991@gmail.com>
2020-03-09 13:29:36 +0800

    Update mics.R

--- R/mics.R
+++ R/mics.R
@@ -9,144 +9,247 @@
 #' @param cell_prop The cellular proportion matrix, where rows correspond to cell types and columns are samples. The default is NA, in which case the cell proportions are estimated from the methylation reference.
 #' @param meth_ref The methylation reference matrix. If cell_prop is NA, meth_ref must be specified, where rows represent CpG sites and columns are cell type annotations.
 #' Its CpG site names are required in order to automatically align with the CpG site names of the observed methylation matrix when estimating cellular compositions.
+#' @param MCP.type The type of multiple comparison procedure when implementing Heller/Sampson method. It is either FWER or FDR. The default is FDR. 
 #' @param cpg_ind The index vector of the informative cpg sites. The default is NA, which means that all cpg sites are used to estimate
 #' the cellular compositions. Otherwise, only the informative cpg sites are used.
 #' @param thd The threshold used to filter out cell types with a small proportion. After estimating the cellular composition for each sample,
 #' we filter out the cell types with its proportions' 0.75 quantile across all samples less than the threshold. The default is 0, which means that no cell type would be removed.
+#' @param maxp_sq A Boolean type argument indicating whether outputs combined p-values using maximum and squaring procedure. The default is FALSE.
 #'
 #' @return A list containing the following items. \item{pval_mediator}{The cell-type-specific p-value matrix by regressing the mediator to the exposure, where rows represent CpG sites and columns are cell types.}
 #' \item{pval_outcome}{The cell-type-specific p-value matrix by regressing the outcome to the mediator and the exposure, where rows represent CpG sites and columns are cell types.}
-#' \item{pval_joint_sq}{The joint cell-type-specific p-value matrix by maximizing and squaring, where rows represent CpG sites and columns are cell types.}
+#' \item{pval_joint_MultiMed}{The joint cell-type-specific p-value matrix using the Heller/Sampson procedure, where rows represent CpG sites and columns are cell types.}
+#' \item{pval_joint_sq}{Only when the argument maxp_sq is TRUE, the returned value is the joint cell-type-specific p-value matrix by maximizing and squaring, where rows represent CpG sites and columns are cell types. Otherwise, it is NA.}
 #' \item{P_matr}{The cell proportion matrix, where rows are cell types and columns correspond to samples.}
 #'
 #' @examples
 #' rm(list = ls())
-#' ###########################################################
-#' #Generate data used in the simulation study
-#' ###########################################################
-#' set.seed(03292019)
+#' set.seed(03092020)
+#' 
+#' #set the working directory
+#' ############################################################################
+#' # Generate Data 
+#' ############################################################################
 #' #Generate samples from the Dirichlet distribution
 #' rDirichlet <- function(alpha_vec){
-#'  num <- length(alpha_vec)
-#'  temp <- rgamma(num, shape = alpha_vec, rate = 1)
-#'  return(temp / sum(temp))
+#'   num <- length(alpha_vec)
+#'   temp <- rgamma(num, shape = alpha_vec, rate = 1)
+#'   return(temp / sum(temp))
 #' }
-#' n <- 400     #number of samples
+#' 
+#' n <- 400     #number of samples 
 #' m <- 10000   #number of CpG sites
 #' K <- 5       #underlying cell type number
-#'
-#' #Generate the baseline methylation profiles mu
-#' #cell type 1
-#' methy1 <- rbeta(m,3,3)
-#'
-#' #cell type 2
-#' methy2 <- methy1 + rnorm(m, sd=0.01)
-#' ind <- sample(1:m, m/10)
-#' methy2[ind] <- rbeta(length(ind),3,3)
-#'
-#' #cell type 3
-#' methy3 <- methy1 + rnorm(m, sd=0.01)
-#' ind <- sample(1:m, m/10)
-#' methy3[ind] <- rbeta(length(ind),3,3)
-#'
-#' #cell type 4
-#' methy4 <- methy1 + rnorm(m, sd=0.01)
-#' ind <- sample(1:m, m/10)
-#' methy4[ind] <- rbeta(length(ind),3,3)
-#'
-#' #cell type 5
-#' methy5 <- methy1 + rnorm(m, sd=0.01)
-#' ind <- sample(1:m, m/10)
-#' methy5[ind] <- rbeta(length(ind),3,3)
-#'
-#' mu_matr <- cbind(methy1, methy2, methy3, methy4, methy5)
-#'
+#' 
+#' ####################################################
 #' #Generate the exposure, confounders, and the outcome
 #' S <- rnorm(n) #the exposure
 #' D <- 2        #the confounder number
 #' X <- matrix(rnorm(D*n), n, D) #confounders
-#'
-#' Y <- 0.5*S + 0.5*X[ ,1] + 0.5*X[ ,2] + rnorm(n, sd = 0.5)
-#'
-#' #Generate the coefficients in Part I
+#' 
+#' ##############################################	
+#' #Generate the baseline methylation profiles mu
+#' #cell type 1
+#' methy1 <- rbeta(m,3,3)
+#' 
+#' #cell type 2 
+#' methy2 <- methy1 + rnorm(m, sd=0.01)
+#' ind <- sample(1:m, m/10) 
+#' methy2[ind] <- rbeta(length(ind),3,3)
+#' 
+#' #cell type 3
+#' methy3 <- methy1 + rnorm(m, sd=0.01)
+#' ind <- sample(1:m, m/10) 
+#' methy3[ind] <- rbeta(length(ind),3,3)
+#' 
+#' #cell type 4
+#' methy4 <- methy1 + rnorm(m, sd=0.01)
+#' ind <- sample(1:m, m/10) 
+#' methy4[ind] <- rbeta(length(ind),3,3)
+#' 
+#' #cell type 5
+#' methy5 <- methy1 + rnorm(m, sd=0.01)
+#' ind <- sample(1:m, m/10) 
+#' methy5[ind] <- rbeta(length(ind),3,3)
+#' 
+#' mu_matr <- cbind(methy1, methy2, methy3, methy4, methy5)
+#' colnames(mu_matr) <- c("celltype 1", "celltype 2", "celltype 3",
+#'                        "celltype 4", "celltype 5")
+#' 
+#' ################################################
+#' #Generate the coefficients
 #' #the effects of the exposure on the mediators
 #' beta_matr <- array(0, dim=c(m,K))
-#'
-#' m_common <- 10
-#' max_signal <- 0.15
-#' min_signal <- 0.07
-#'
-#' signs <- sample(c(-1,1), m_common*K, replace=T)
-#' beta_matr[1:m_common,1:K] <- signs * runif(m_common*K, min=min_signal, max=max_signal)
-#'
-#' m_seperate <- 10
-#' signs <- sample(c(-1,1), m_seperate*2, replace=T)
-#' beta_matr[m_common+(1:m_seperate),1:2] <- signs * runif(m_seperate*2, min=min_signal, max=max_signal)
-#'
-#' for(k in 3:K){
-#'   signs <- sample(c(-1,1), m_seperate, replace=T)
-#'  beta_matr[m_common+(k-2)*m_seperate+(1:m_seperate),k] <- signs * runif(m_seperate, min=min_signal, max=max_signal)
-#' }
-#'
+#' 
+#' #the cpg sites affected by exposure for each cell type
+#' cpg_type1 <- 1:75
+#' cpg_type2 <- 26:100
+#' cpg_type3 <- 51:125
+#' cpg_type4 <- 76:150
+#' cpg_type5 <- 101:175
+#' 
+#' #coefficients
+#' max_signal <- 0.25
+#' min_signal <- 0.1
+#' 
+#' signs <- sample(c(-1,1), 75, replace=T)
+#' beta_matr[cpg_type1, 1] <- signs * runif(75, min=min_signal, max=max_signal)
+#' 
+#' signs <- sample(c(-1,1), 75, replace=T)
+#' beta_matr[cpg_type2, 2] <- signs * runif(75, min=min_signal, max=max_signal)
+#' 
+#' signs <- sample(c(-1,1), 75, replace=T)
+#' beta_matr[cpg_type3, 3] <- signs * runif(75, min=min_signal, max=max_signal)
+#' 
+#' signs <- sample(c(-1,1), 75, replace=T)
+#' beta_matr[cpg_type4, 4] <- signs * runif(75, min=min_signal, max=max_signal)
+#' 
+#' signs <- sample(c(-1,1), 75, replace=T)
+#' beta_matr[cpg_type5, 5] <- signs * runif(75, min=min_signal, max=max_signal)
+#' 
 #' #the effects of the confounders on the mediators
 #' h_vec1 <- rnorm(m, sd=0.01)
 #' h_vec2 <- rnorm(m, sd=0.01)
-#'
-#' #generate the cellular compositions
-#' P_matr <- sapply(1:n, function(i){
-#'   rDirichlet(rep(2, K))
-#' })
-#'
+#' 
+#' 
+#' ##################################################
 #' #Generate the effect of mediators on the outcome
-#' gamma_tilde_matr <- matrix(0, m, K)
-#' for(k in 1:K){
-#'   signs <- sample(c(-1,1), m_seperate, replace=T)
-#'   gamma_tilde_matr[(k-1)*m_seperate+1:m_seperate, k] <- signs * runif(m_seperate, min=0.03, max=0.1)
-#'   if(k == 3){
-#'     signs <- sample(c(-1,1), m_seperate, replace=T)
-#'     gamma_tilde_matr[(k-3)*m_seperate+1:m_seperate, k] <- signs * runif(m_seperate, min=0.03, max=0.1)
-#'   }
-#' }
-#'
+#' gamma_matr <- matrix(0, m, K)
+#' max_signal_gamma <- 0.035
+#' min_signal_gamma <- 0.025
+#' 
+#' #the cpg sites affected by mediators for each cell type
+#' cpg_type1_m <- 1:50
+#' cpg_type2_m <- 26:75
+#' cpg_type3_m <- 51:100
+#' cpg_type4_m <- 76:125
+#' cpg_type5_m <- 101:150
+#' 
+#' signs <- sample(c(-1,1), 50, replace=T)
+#' gamma_matr[cpg_type1_m, 1] <- signs * runif(50, min=min_signal_gamma, max=max_signal_gamma)
+#' 
+#' signs <- sample(c(-1,1), 50, replace=T)
+#' gamma_matr[cpg_type2_m, 2] <- signs * runif(50, min=min_signal_gamma, max=max_signal_gamma)
+#' 
+#' signs <- sample(c(-1,1), 50, replace=T)
+#' gamma_matr[cpg_type3_m, 3] <- signs * runif(50, min=min_signal_gamma, max=max_signal_gamma)
+#' 
+#' signs <- sample(c(-1,1), 50, replace=T)
+#' gamma_matr[cpg_type4_m, 4] <- signs * runif(50, min=min_signal_gamma, max=max_signal_gamma)
+#' 
+#' signs <- sample(c(-1,1), 50, replace=T)
+#' gamma_matr[cpg_type5_m, 5] <- signs * runif(50, min=min_signal_gamma, max=max_signal_gamma)
+#' 
+#' 
+#' #######################################################
+#' #Generate the mediators and the outcome 
+#' 
+#' #the effects of the confounders on the outcome
+#' eta1 <- 0.5
+#' eta2 <- 0.5
+#' 
+#' #the effects of the exposure on the outcome
+#' eta0 <- 0.5
+#' 
+#' #jointly generating u and Y is equivalent to first generating
+#' # Y and then generating u conditional on Y
+#' 
+#' Y <- 0.5*S + 0.5*X[ ,1] + 0.5*X[ ,2] + rnorm(n, sd = 0.5)
+#' 
+#' 
 #' #Generate u conditional on Y
 #' u <- array(NA, dim=c(n, m, K))
 #' for(i in 1:n){
-#'   epsilon_matr <- matrix(rnorm(m*K, sd = 0.015), m, K)
-#'   #epsilon_matr <- matrix(c(rnorm(m*2, sd = 0.02), rnorm(m*2, sd = 0.01), rnorm(m, sd = 0.015)), m, K)
-#'   u[i,,] <- mu_matr + beta_matr*S[i] + h_vec1*X[i,1] + h_vec2*X[i,2] + gamma_tilde_matr * Y[i] + epsilon_matr
+#'   epsilon_matr <- matrix(rnorm(m*K, sd = 0.01), m, K)
+#'   u[i,,] <- mu_matr + beta_matr*S[i] + h_vec1*X[i,1] + h_vec2*X[i,2] + 
+#'   gamma_matr*Y[i] + epsilon_matr  
 #' }
-#'
-#' #Generate the observed methylation profiles
+#' 
+#' #############################################
+#' #Generate the cellular compositions 
+#' P_matr <- sapply(1:n, function(i){
+#'   rDirichlet(c(4, 1, 3, 1, 1))
+#' })
+#' 
+#' #############################################
+#' #Generate the observed methylation profiles 
 #' Ometh <- NULL
 #' for(i in 1:n){
 #'   tmp <- u[i,,] %*% P_matr[ ,i]
 #'   Ometh <- cbind(Ometh, tmp)
 #' }
-#'
-#' sum(Ometh > 1)
+#' 
+#' #sum(Ometh > 1)
 #' Ometh[Ometh > 1] <- 1
-#'
-#' sum(Ometh < 0)
+#' 
+#' #sum(Ometh < 0)
 #' Ometh[Ometh < 0] <- 0
-#'
+#' 
 #' rownames(Ometh) <- paste0("cpg", 1:m)
 #' rownames(mu_matr) <- paste0("cpg", 1:m)
-#' colnames(mu_matr) <- paste0("cell_type", 1:K)
-#'
-#' ###########################################################
-#' #Conduct MICS
-#' ###########################################################
-#'
-#' t1 <- Sys.time()
-#' out <- mics(Ometh, S, X, Y, meth_ref = mu_matr)
-#' pval_joint_sq <- out$pval_joint_sq
-#' t2 <- Sys.time()
-#' print(t2 - t1)
+#' 
+#' 
+#' ############################################################################
+#' # Conduct MICS 
+#' ############################################################################
+#' 
+#' 
+#' #install MICS package first
+#' library(MICS)
+#' 
+#' out <- mics(meth_data = Ometh, S = S, X = X, Y = Y, meth_ref = mu_matr, 
+#'             MCP.type = "FDR", maxp_sq = TRUE)
+#' 
+#' out$pval_joint_sq[1:100, 1]
+#' 
+#' 
+#' pval_MultiMed <- out$pval_joint_MultiMed
+#' K <- 5
+#' m <- 10000
+#' 
+#' fdr_thred <- 0.2
+#' 
+#' #the true cpg sites through the mediation pathway 
+#' cpg_type1 <- 1:50
+#' cpg_type2 <- 26:75
+#' cpg_type3 <- 51:100
+#' cpg_type4 <- 76:125
+#' cpg_type5 <- 101:150
+#' 
+#' ###cell type 1
+#' #power 
+#' sum(pval_MultiMed[cpg_type1, 1] < fdr_thred) / 50
+#' #false positive rate
+#' sum(pval_MultiMed[setdiff(1:m, cpg_type1), 1] < fdr_thred) / (m-50)
+#' 
+#' ###cell type 2
+#' #power 
+#' sum(pval_MultiMed[cpg_type2, 2] < fdr_thred) / 50
+#' #false positive rate
+#' sum(pval_MultiMed[setdiff(1:m, cpg_type2), 2] < fdr_thred) / (m-50)
+#' 
+#' ###cell type 3
+#' #power 
+#' sum(pval_MultiMed[cpg_type3, 3] < fdr_thred) / 50
+#' #false positive rate
+#' sum(pval_MultiMed[setdiff(1:m, cpg_type3), 3] < fdr_thred) / (m-50)
+#' 
+#' ###cell type 4
+#' #power 
+#' sum(pval_MultiMed[cpg_type4, 4] < fdr_thred) / 50
+#' #false positive rate
+#' sum(pval_MultiMed[setdiff(1:m, cpg_type4), 4] < fdr_thred) / (m-50)
+#' 
+#' ###cell type 5
+#' #power 
+#' sum(pval_MultiMed[cpg_type5, 5] < fdr_thred) / 50
+#' #false positive rate
+#' sum(pval_MultiMed[setdiff(1:m, cpg_type5), 5] < fdr_thred) / (m-50)
+#' 
+#' 
 
 
-
-
-mics <- function(meth_data, S, X, Y, cell_prop = NA, meth_ref = NA, cpg_ind = NA, thd = 0){
+mics <- function(meth_data, S, X, Y, cell_prop = NA, meth_ref = NA, MCP.type = "FDR", cpg_ind = NA, thd = 0, maxp_sq = FALSE){
   if(is.na(cell_prop)[1]&is.na(meth_ref)[1]){
     stop("Either cell_prop or meth_ref should be specified!\n")
   }
@@ -302,30 +405,57 @@ mics <- function(meth_data, S, X, Y, cell_prop = NA, meth_ref = NA, cpg_ind = NA
 	colnames(pval_mediator) <- rownames(P_matr)
 	rownames(pval_mediator) <- rownames(Ometh)
 
-  #pval_outcome
+	#pval_outcome
 	pval_outcome <- gamma_tilde_pval
 	colnames(pval_outcome) <- rownames(P_matr)
 	rownames(pval_outcome) <- rownames(Ometh)
 
-	#pval_joint_sq
-	pval_joint <- matrix(NA, m, K)
-	for(j in 1:m){
-	  for(k in 1:K){
-	    pval_joint[j,k] <- max(beta_pval[j,k], gamma_tilde_pval[j,k])
-	  }
+
+	if(maxp_sq == TRUE){
+		#pval_joint_sq
+		pval_joint <- matrix(NA, m, K)
+		for(j in 1:m){
+			for(k in 1:K){
+				pval_joint[j,k] <- max(beta_pval[j,k], gamma_tilde_pval[j,k])
+			}
+		}
+		pval_joint_sq <- pval_joint^2
+		colnames(pval_joint_sq) <- rownames(P_matr)
+		rownames(pval_joint_sq) <- rownames(Ometh)
+	}else{
+		pval_joint_sq <- NA
 	}
 
-	pval_joint_sq <- pval_joint^2
-	colnames(pval_joint_sq) <- rownames(P_matr)
-	rownames(pval_joint_sq) <- rownames(Ometh)
-
+	if(MCP.type == "FDR"){
+		pval_joint_MultiMed <- NULL
+		for(k in 1:K){
+			tmp <- medTest.SBMH(as.vector(pval_mediator[ ,k]), as.vector(pval_outcome[ ,k]), MCP.type = "FDR")
+			tmp <- as.vector(tmp)
+			pval_joint_MultiMed <- cbind(pval_joint_MultiMed, tmp)
+		}
+		colnames(pval_joint_MultiMed) <- rownames(P_matr)
+		rownames(pval_joint_MultiMed) <- rownames(Ometh)
+	}else if(MCP.type == "FWER"){
+		pval_joint_MultiMed <- NULL
+		for(k in 1:K){
+			tmp <- medTest.SBMH(as.vector(pval_mediator[ ,k]), as.vector(pval_outcome[ ,k]), MCP.type = "FWER")
+			tmp <- as.vector(tmp)
+			pval_joint_MultiMed <- cbind(pval_joint_MultiMed, tmp)
+		}
+		colnames(pval_joint_MultiMed) <- rownames(P_matr)
+		rownames(pval_joint_MultiMed) <- rownames(Ometh)
+	}else{
+	    stop("MCP.type must be specified as either \"FDR\" or \"FWER\"!\n")
+	}
+	
 
 	#return values
-  out <- list()
-  out$pval_mediator <- pval_mediator
-  out$pval_outcome <- pval_outcome
-  out$pval_joint_sq <- pval_joint_sq
-
-  out$P_matr <- P_matr
+	out <- list()
+	out$pval_mediator <- pval_mediator
+	out$pval_outcome <- pval_outcome
+	out$pval_joint_sq <- pval_joint_sq
+	out$pval_joint_MultiMed <- pval_joint_MultiMed
+	
+	out$P_matr <- P_matr
 	return(out)
 }
